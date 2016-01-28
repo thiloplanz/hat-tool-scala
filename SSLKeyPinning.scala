766765ec1a14f6dd18bc9c97114ebdb5840fe744
Thilo Planz <thilo@cpan.org>
2016-01-28 08:39:14 +0800

    add option to "skipX509ExtendedTrustVerification" (i.e. host name checking)

--- SSLKeyPinning.scala
+++ SSLKeyPinning.scala
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, Thilo Planz.
+// Copyright (c) 2015/2016, Thilo Planz.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the Apache License, Version 2.0
@@ -15,13 +15,11 @@
 // If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
 
-import java.net.{Socket, InetAddress}
+import java.net.Socket
 import java.security.cert.{CertificateException, X509Certificate}
-import javax.net.ssl.{SSLSocketFactory, X509TrustManager, SSLContext}
+import javax.net.ssl._
 
 import org.apache.commons.codec.digest.DigestUtils
-
-import scala.collection.mutable.ArrayBuffer
 
 
 /**
@@ -32,23 +30,23 @@ import scala.collection.mutable.ArrayBuffer
  * The certificate is identified by the SHA-256 hash of its SSL certificate.
  *
  * <code>
- * new SSLKeyPinning( sha256HexDigestOfServerCert ).setDefaultSSLContext()
+ * SSLKeyPinning( sha256HexDigestOfServerCert ).setDefaultSSLContext()
  * // continue to use HTTPS clients normally
  * </code>
  *
  */
 
 
-class SSLKeyPinning(trustedDigest: String = null, socketFactory : SSLSocketFactory = SSLContext.getDefault().getSocketFactory) extends SSLSocketFactory with X509TrustManager  {
-
-  private val trusted : ArrayBuffer[String] = if (trustedDigest == null) ArrayBuffer() else ArrayBuffer(trustedDigest)
+class SSLKeyPinning private (trustedDigests: Seq[String])   {
 
 
   /**
-   * add another pinned certificate
+   * create an SSLContext that implements the key pinning
    */
-  def addTrusted(digest: String): Unit ={
-    trusted += digest
+  def makeSSLContext(skipX509ExtendedTrustVerification: Boolean = false) = {
+    val sslContext = SSLContext.getInstance("SSL")
+    sslContext.init(null, Array(if (skipX509ExtendedTrustVerification) new ExtendedTrustManager else new TrustManager {}), null)
+    sslContext
   }
 
 
@@ -56,56 +54,51 @@ class SSLKeyPinning(trustedDigest: String = null, socketFactory : SSLSocketFacto
    * enable the key pinning by setting the default SSLContext.
    *
    * This is a system-wide setting.
-   * There may be less intrusive ways if you control over how
+   * There may be less intrusive ways if you control how
    * your HTTPS client is configured.
    */
-  def setDefaultSSLContext() {
-    val sslContext = SSLContext.getInstance("SSL")
-    sslContext.init(null, Array(this), null)
-    SSLContext.setDefault(sslContext)
-  }
+  def setDefaultSSLContext(skipX509ExtendedTrustVerification: Boolean = false) = SSLContext.setDefault(makeSSLContext(skipX509ExtendedTrustVerification))
 
 
-
-
-  // implementation of X509TrustManager
-
-  override def checkClientTrusted(chain: Array[X509Certificate], authType: String)=  {
-    throw new CertificateException("Key pinning is only implemented for server certificates")
-  }
-
-  override def checkServerTrusted(chain: Array[X509Certificate], authType: String) {
-    val serverCert = chain(0)
-    val digest = DigestUtils.sha256Hex(serverCert.getEncoded)
-    for(t <- trusted){
-       if (t.equals(digest)) return;
+  // implementation of X509TrustManager. Using this results in the system providing the default extended trust verification code
+  private trait TrustManager extends X509TrustManager {
+    override def checkClientTrusted(chain: Array[X509Certificate], authType: String) = {
+      throw new CertificateException("Key pinning is only implemented for server certificates")
     }
-    // TODO: public key pinning (instead of certificate pinning)
-    throw new CertificateException("Server certificate for "+serverCert.getSubjectX500Principal.getName+" has not been pinned. Found sha256 digest of "+digest)
+
+    override def checkServerTrusted(chain: Array[X509Certificate], authType: String) {
+      val serverCert = chain(0)
+      val digest = DigestUtils.sha256Hex(serverCert.getEncoded)
+      if (trustedDigests.contains(digest)) return;
+      // TODO: public key pinning (instead of certificate pinning)
+      throw new CertificateException("Server certificate for " + serverCert.getSubjectX500Principal.getName + " has not been pinned. Found sha256 digest of " + digest)
+    }
+
+    override def getAcceptedIssuers(): Array[X509Certificate] = {
+      return Array()
+    }
   }
 
-  override def getAcceptedIssuers() : Array[X509Certificate] = {
-    return Array()
+
+  // implementation of X509ExtendedTrustManager (that just skips the extended trust verification, such as hostname checking)
+  private class ExtendedTrustManager extends X509ExtendedTrustManager with TrustManager{
+
+    // we don't do the additional checks at all
+    override def checkClientTrusted(chain: Array[X509Certificate], authType: String, socket: Socket): Unit = checkClientTrusted(chain, authType)
+
+    override def checkClientTrusted(chain: Array[X509Certificate], authType: String, sslEngine: SSLEngine): Unit  = checkClientTrusted(chain, authType)
+
+    override def checkServerTrusted(chain: Array[X509Certificate], authType: String, socket: Socket): Unit  = checkServerTrusted(chain, authType)
+
+    override def checkServerTrusted(chain: Array[X509Certificate], authType: String, sslEngine: SSLEngine): Unit  = checkServerTrusted(chain, authType)
   }
-
-  // implementation of SSLSocketFactory
-  // (just delegates to the default socketFactory, we just supply a TrustManager)
-
-  override def getDefaultCipherSuites: Array[String] = socketFactory.getDefaultCipherSuites
-
-  override def getSupportedCipherSuites: Array[String] = socketFactory.getDefaultCipherSuites
-
-  override def createSocket(socket: Socket, s: String, i: Int, b: Boolean): Socket = socketFactory.createSocket(socket, s, i, b)
-
-  override def createSocket(s: String, i: Int): Socket = socketFactory.createSocket(s,i)
-
-  override def createSocket(s: String, i: Int, inetAddress: InetAddress, i1: Int): Socket = socketFactory.createSocket(s, i, inetAddress, i1)
-
-  override def createSocket(inetAddress: InetAddress, i: Int): Socket = socketFactory.createSocket(inetAddress, i)
-
-  override def createSocket(inetAddress: InetAddress, i: Int, inetAddress1: InetAddress, i1: Int): Socket = socketFactory.createSocket(inetAddress, i, inetAddress1, i1)
-
 
 
 }
+
+object SSLKeyPinning {
+  def apply(trustedDigests: Iterable[String]) = new SSLKeyPinning(trustedDigests.toSeq)
+  def apply(trustedDigests: String*) = new SSLKeyPinning(trustedDigests.toSeq)
+}
+
 
